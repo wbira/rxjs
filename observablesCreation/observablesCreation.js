b7e7c7b1f29a07d80ba1f4bfee0518e8fcb13425
Waldemar Bira <waldek.bira@gmail.com>
2017-10-17 15:11:28 +0200

    Exercise wiith fromEventPattern operator

--- observablesCreation/observablesCreation.js
+++ observablesCreation/observablesCreation.js
@@ -1,6 +1,6 @@
 //ex 1
 var firstObservable = Rx.Observable.create(function (observer) {
-  return observer.next(42);
+  observer.next(42);
 });
 
 firstObservable.subscribe(console.log);
@@ -35,5 +35,37 @@ errorObservable.subscribe(console.log, console.error)
 //ex 4
 
 //of operator emit values and then completes
-var obs1 = Rx.Observable.of(42,100,200);
+var obs1 = Rx.Observable.of(42, 100, 200);
 obs1.subscribe(console.log)
+
+//ex 5
+
+var obsArray = Rx.Observable.from([42, 100, 200]);
+obsArray.subscribe(console.log)
+
+
+//ex 6
+
+var todosPromise = fetch('https://jsonplaceholder.typicode.com/todos').then(res => res.json());
+var obsPromise = Rx.Observable.fromPromise(todosPromise); //from also works
+obsPromise.subscribe(console.log);
+
+//ex 7 events
+
+function addClickHandler(handler) {
+  document.addEventListener('click', handler);
+}
+
+function removeClickHandler(handler) {
+  console.log("remove handler")
+  document.removeEventListener('click', handler);
+}
+
+var clicks = Rx.Observable.fromEventPattern(
+  addClickHandler,
+  removeClickHandler
+);
+
+var clicksSubscription = clicks.subscribe(console.log);
+
+setTimeout(() => { clicksSubscription.unsubscribe()}, 5000)
